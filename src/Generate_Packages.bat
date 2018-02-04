81a776c09bef030212a468f0a4c841e767d889c0
sm_d3po <seanmarek@gmail.com>
2018-02-03 22:40:54 -0800

    heh left a them as direct dependencies, that was dumb

---
+++ src/Generate_Packages.bat
@@ -0,0 +1,3 @@
+nuget pack SMT.Tracing.Core/SMT.Tracing.Core.csproj -Prop Configuration=Release
+nuget pack SMT.Tracing.AspContext/SMT.Tracing.AspContext.csproj -Prop Configuration=Release
+nuget pack SMT.Tracing.HttpClient/SMT.Tracing.HttpClient.csproj -Prop Configuration=Release
